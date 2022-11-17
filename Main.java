84ac96651247230cf1a900adc3a1285df4edc1df
joao_paulo_oliveira <vigjoaopaulo@gmail.com>
2022-11-17 09:12:01 +0000

    mostrando letra digitada na forca

--- Main.java
+++ Main.java
@@ -13,22 +13,44 @@ class Main {
   public static void main(String[] args) throws IOException{
     Scanner sc = new Scanner(System.in);
     Random random = new Random();
-    String[] palavras = {"BICICLETA", "AMORA", "MACARRONADA", "FEITIÇARIA","AMIGO", "PALAVRA", "CAVALEIRO"};
-
+    String letrasUltilizada = "";
+    char letra;
     
-    int idx = palavras.length; //PEGA QTS PALAVRAS TEM NO VETOR
-    System.out.println("idx:"+ idx);
+    
+    String[] palavras = {"BICICLETA", "AMORA", "MACARRONADA", "FEITIÇARIA","AMIGO", "PALAVRA", "CAVALEIRO"};
+    
+    int qtdPalavras = palavras.length; //PEGA QTD PALAVRAS TEM NO VETOR
+    System.out.println("Quantidade de palavras: "+ qtdPalavras);
 
-   
-    int tam = palavras[1].length();  //PEGA QUANTAS LETRA TEM A PALAVRA
-    System.out.println("tamanho da palavra: "+ tam);
+    int idxSorteado = random.nextInt(qtdPalavras); //POSICAO NO VETOR QUE DA A PALAVRA SORTEADA
+    String sorteada = palavras[idxSorteado];//PALAVRA SORTEADA PALAVRA NA POSICAO idxSorteado
+    
+    int tam = sorteada.length();  //PEGA QUANTAS LETRA TEM A PALAVRA
+    char[] acertos =new char[tam];
 
-    for(int i = 0; i < tam; i++){
-      System.out.print(" - ");
+    for(int i = 0; i < acertos.length; i++){
+      acertos[i] = 0;
     }
+    System.out.println("Palavra selecionada: " + sorteada);
+    System.out.println("Quantidade de letras: " + tam);  
+    System.out.println("");
+    
+
+    System.out.println("\nQual letra vc deseja tentar?");
+    letra = sc.next().toUpperCase().charAt(0);
 
     for(int i = 0; i < tam; i++){
-
+      if(letra == sorteada.charAt(i)){
+        
+        acertos[i] = 1;
+      }
+    }
+    for(int i = 0; i < tam; i++){
+      if(acertos[i] == 0){
+        System.out.print(" - ");
+      }else{
+        System.out.print("  " + sorteada.charAt(i) + " ");
+      }
     }
   }
 }
