7a95875051912b6920299c2bcb9e6dead9dec38d
joao_paulo_oliveira <vigjoaopaulo@gmail.com>
2022-11-18 02:18:28 +0000

    subtraindo vidas

--- Main.java
+++ Main.java
@@ -20,7 +20,7 @@ class Main {
     String[] palavras = {"BICICLETA", "AMORA", "MACARRONADA", "FEITIÇARIA","AMIGO", "PALAVRA", "CAVALEIRO"};
     
     int qtdPalavras = palavras.length; //PEGA QTD PALAVRAS TEM NO VETOR
-    System.out.println("Quantidade de palavras: "+ qtdPalavras);
+    
 
     int idxSorteado = random.nextInt(qtdPalavras); //POSICAO NO VETOR QUE DA A PALAVRA SORTEADA
     String sorteada = palavras[idxSorteado];//PALAVRA SORTEADA PALAVRA NA POSICAO idxSorteado
@@ -31,36 +31,52 @@ class Main {
     for(int i = 0; i < acertos.length; i++){
       acertos[i] = 0;
     }
+    System.out.println("\n----------- JOGO DA FORCA ---------");
+    System.out.println("Quantidade de palavras: "+ qtdPalavras);
     System.out.println("Palavra selecionada: " + sorteada);
     System.out.println("Quantidade de letras: " + tam);  
-    System.out.println("");
+    System.out.println("-----------------------------------");
     
     boolean ganhou = false;
     int vidas = tam;
-    
-  while(!ganhou && vidas > 0){
-    System.out.println("\nVocê tem " + vidas + "vidas");
+  for(int i = 0; i < tam; i++){
+      if(acertos[i] == 0){
+        System.out.print(" - ");
+        
+      }
+    }
+  while(!ganhou ){
+    System.out.println("\nVocê tem " + vidas + " vidas");
     System.out.println("Letras utilizadas: " + letrasUltilizada);
     System.out.println("Qual letra vc deseja tentar?");
     letra = sc.next().toUpperCase().charAt(0);
     letrasUltilizada+= " " + letra;
-
+    boolean perderVida = false;
     for(int i = 0; i < tam; i++){
       if(letra == sorteada.charAt(i)){
-        
+       
         acertos[i] = 1;
+      }else{
+        perderVida = true;
       }
     }
+
+    
     for(int i = 0; i < tam; i++){
       if(acertos[i] == 0){
         System.out.print(" - ");
+                 
         ganhou = false;
       }else{
         System.out.print("  " + sorteada.charAt(i) + " ");
       }
     }
+
+    if(perderVida){
+      vidas--;
+    }
   }
-    System.out.print("Parabéns vc ganhou!!!");
+    System.out.print("\nParabéns vc ganhou!!!");
 
     /*if(ganhou > 0){
       System.out.print("Parabéns vc ganhou!!!");
